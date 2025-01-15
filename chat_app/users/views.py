62825e63cd1214085a85b6ca168632876f4461f0
PuruPanda1 <purabmodi2002@gmail.com>
2025-01-16 02:16:13 +0530

    Implemented chat Screen

--- chat_app/users/views.py
+++ chat_app/users/views.py
@@ -16,11 +16,11 @@ def login_page(request):
         if user is not None:
             login(request, user)
             messages.success(request, 'Login successful!')
-            return redirect('/chat/admin/')
+            return redirect('/chat/random/')
         else:
             messages.error(request, 'Invalid email or password. Please try again.')
     if request.user.is_authenticated:
-        return redirect('/chat/admin/')
+        return redirect('/chat/random/')
     return render(request,'login.html')
 
 
@@ -57,6 +57,6 @@ def signup_view(request):
         messages.success(request, 'Signup successful! You can now log in.')
         return redirect('login')
     if request.user.is_authenticated:
-        return redirect('/chat/admin/') #todo: pass usename
+        return redirect('/chat/random/') #todo: pass usename
     return render(request, 'signup.html')
 
